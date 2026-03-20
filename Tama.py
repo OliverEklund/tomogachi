7f189bee5cb0081445862fe275f2c6cef1162104
OliverEklund <156414019+OliverEklund@users.noreply.github.com>
2026-03-20 11:10:50 +0100

    ahhhhhhhhhhhhhhhhhhhhhhhhh

---
+++ Tama.py
@@ -0,0 +1,8 @@
+class Tamagochi: 
+    def __init__(self, name, health, happiness, hunger, mess, age):
+        self.name = input("Vad ska dit djur heta?: ")
+        self.health = 100
+        self.happiness = 30
+        self.hunger = 50
+        self.mess = 0
+        self.age = 1
