aed04bc88cf76d907e5e779589d53f7c40eadb8a
OliverEklund <156414019+OliverEklund@users.noreply.github.com>
2026-06-03 13:24:23 +0200

    Life, death, rebirth

--- Tama.py
+++ Tama.py
@@ -1,8 +1,55 @@
+from random import randint
+
 class Tamagochi: 
-    def __init__(self, name, health, happiness, hunger, mess, age):
+    def __init__(self, happiness = 30, hunger = 50, mess = 0, age = 1, alive = True):
         self.name = input("Vad ska dit djur heta?: ")
-        self.health = 100
-        self.happiness = 30
-        self.hunger = 50
-        self.mess = 0
-        self.age = 1
+        self.happiness = happiness
+        self.hunger = hunger
+        self.mess = mess
+        self.age = age
+        self.alive = alive
+        
+    
+    def action(self):
+        while self.alive == True:
+
+            print(f"Namn: {self.name}, Ålder: {self.age}, Glädje: {self.happiness}, Hunger: {self.hunger}, Smutsighet: {self.mess}")
+        
+            choice = input("Vad vill du göra? Skriv nummer för att välja, enter för att ignorera. 1:Leka 2:Städa 3:Mata - ")
+            if choice == "1":
+                self.happiness += 20
+                print(f"{self.name} blev glad av att leka!")
+
+            elif choice == "2":
+                self.mess -= 2
+                print(f"{self.name} blev renare!")
+
+            elif choice == "3":
+                self.hunger += 40
+                print(f"{self.name} blev mättare!")
+
+            else:
+                print(f"Du ignorerar {self.name}")
+
+            self.age += 1
+            self.hunger -= 10
+            self.happiness -= 10
+            self.mess += 1
+
+            if randint(1,50) == 1:
+                self.alive = False
+                print(f"{self.name} Hade en olycka och dog")
+            if self.hunger == 0:
+                self.alive = False
+                print(f"{self.name} svalt till döds")
+            if self.happiness == 0:
+                self.alive = False
+                print(f"{self.name} blev deppad och dog")
+            if self.mess == 10:
+                self.alive = False
+                print(f"{self.name} blev sjuk och dog")
+
+
+while True:
+    pet = Tamagochi()
+    Tamagochi.action(pet)
--- test.py
+++ test.py
@@ -1,3 +1,9 @@
-import Tama
+def input_int(prompt):
+    while True:
+        try:
+            return int(input(prompt))
+        except ValueError and int != 1 and int != 2 and int !=3 and int != 4:
+            print("Invalid input.")
 
-Tamagochi()
+
+input_int("hej")
